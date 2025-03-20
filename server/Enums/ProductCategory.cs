6a6a0d16f3fe68c41e9c565cdbf3424f819a0220
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2025-03-20 09:14:39 +0530

    Refactor ProductCategory to use enum type
    Replaced ProductCategory property in Product class from string to ProductCategory enum.
    Introduced new ProductCategory enum in ProductCategory.cs under server.Enums namespace.
    Updated Product.cs to include using directive for server.Enums.

---
+++ server/Enums/ProductCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace server.Enums
+{
+    public enum ProductCategory
+    {
+        Electronics,
+        Clothing,
+        HomeAppliance,
+        Books,
+        Toys,
+        Shoes,
+        Beauty
+    }
+}
--- server/Models/Entities/Product.cs
+++ server/Models/Entities/Product.cs
@@ -1,11 +1,13 @@
-﻿namespace server.Models.Entities
+﻿using server.Enums;
+
+namespace server.Models.Entities
 {
     public class Product
     {
         public int ProductId { get; set; }
         public required string ProductName { get; set; }
         public required string ProductDescription { get; set; }
-        public required string ProductCategory { get; set; }
+        public required ProductCategory ProductCategory { get; set; }
         public required decimal ProductPrice { get; set; }
         public required int ProductQuantity { get; set; }
     }
