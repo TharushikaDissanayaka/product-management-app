bde4821d5c6670fdd026fa6635c309843d5c7253
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2025-03-20 09:50:46 +0530

    Add ApplicationDbContext with DbSet<Product> for EF Core
    Introduced ApplicationDbContext in server.Data namespace, inheriting from DbContext. Configured with DbContextOptions and added DbSet<Product> for managing Product entities. Included necessary using directives for EF Core and server.Models.Entities.

---
+++ server/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Models.Entities;
+
+namespace server.Data
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
