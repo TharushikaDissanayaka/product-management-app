93d43283d0802f8d14bde350f8b11abd040101eb
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2025-03-20 08:50:23 +0530

    Add analytics config, EF Core packages, and Product class
    Updated `angular.json` to include Angular CLI analytics config.
    Modified `server.csproj` to add EF Core packages and a new "Controllers" folder.
    Added `Product` class to `server.Models.Entities` with required properties.

---
+++ server/Models/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace server.Models.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public required string ProductName { get; set; }
+        public required string ProductDescription { get; set; }
+        public required string ProductCategory { get; set; }
+        public required decimal ProductPrice { get; set; }
+        public required int ProductQuantity { get; set; }
+    }
+}
