81c7fabd7b14d0f871ce7b8d7584c642257fe47c
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2025-03-21 09:47:49 +0530

    Add dependency injection and POST method to ProductsController
    The ProductsController class now has a constructor that takes an
    ApplicationDbContext parameter, enabling dependency injection for
    database operations. A private readonly field _context has been
    added to store the ApplicationDbContext instance. Additionally,
    a new PostProduct method has been added to handle HTTP POST
    requests, allowing clients to add new products to the database
    and returning the added product in the response.

--- server/Controllers/ProductsController.cs
+++ server/Controllers/ProductsController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using server.Data;
+using server.Models.Entities;
 
 namespace server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController(ApplicationDbContext context) : ControllerBase
     {
+        private readonly ApplicationDbContext _context = context;
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return Ok(product);
+        }
     }
 }
