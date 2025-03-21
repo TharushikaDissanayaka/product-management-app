299388d14645c621fab618696d419a6836cabf19
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2025-03-21 11:03:27 +0530

    Add ProductAdd component and integrate it into the main app layout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductAddComponent } from "./pages/product-add/product-add.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductAddComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
