74ee39159128b5b21a2f7782479eb070a5f08ace
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2025-03-21 10:59:20 +0530

    Create Product interface model

---
+++ client/src/models/product.model.ts
@@ -0,0 +1,8 @@
+export interface Product {
+    productId: number;
+    ProductName: string;
+    ProductDescription: string;
+    ProductCategory: number;
+    ProductPrice: number;
+    ProductQuantity: number;
+}
